58f138ef7b7d10301f59a5682c6919a3dfae767f
184885 <184885@stud.hvl.no>
2025-03-20 15:07:05 +0100

    Fjernet HobbyMatchMain og endret navn på alt...

--- src/no/hvl/dat102/O4/HobbyMatchMain.java
+++ src/no/hvl/dat102/O4/HobbyMatchMain.java
@@ -2,63 +2,29 @@ package no.hvl.dat102.O4;
 
 public class HobbyMatchMain {
 	public static void main(String args[]) {
-		System.out.println("Match Ola med Kari:" + match(new Person("Ola", "Fotball", "Data"), new Person("Kari", "Fotball")));
-		System.out.println("Match Kari med Ola:" + match(new Person("Kari", "Fotball"), new Person("Ola", "Fotball", "Data")));
-		System.out.println("Match Ola med Ola:" + match(new Person("Ola", "Fotball", "Data"), new Person("Ola", "Fotball", "Data")));
-		System.out.println("Match Kari med Kari:" + match(new Person("Kari", "Fotball"), new Person("Kari", "Fotball")));
+		Person arne = new Person("Arne", "jakt", "sykling", "venner", "data");
+		Person per = new Person("Per", "venner", "sjakk", "data");
+		Person paal = new Person("Pål", "data", "lese", "sport");
+//		Person paal = new Person("Pål");
+		
+		double matchArneOgPer = match(arne, per);
+		double matchPerOgArne = match(per, arne);
+		double matchArneOgPaal = match(arne, paal);
+		double matchPerOgPaal = match(per, paal);
+		
+		System.out.println("Match mellom "+ arne.getNavn() + " og " + per.getNavn() + " er "  + matchArneOgPer);
+		System.out.println("Match mellom "+ per.getNavn() + " og " + arne.getNavn() + " er "  + matchPerOgArne);
+		System.out.println("Match mellom " + arne.getNavn() + " og " + paal.getNavn() + " er " + matchArneOgPaal);
+		System.out.println("Match mellom " + per.getNavn()+ " og " + paal.getNavn() + " er " + matchPerOgPaal);
 	}
-	
-	static double match(Person a, Person b) {
-		// match = antallFelles - (antallKunHosA + antallKunHosB)/antallTotalt
-//		TabellMengde<Integer> felles = new TabellMengde<Integer>();
-//		TabellMengde<Integer> kunHosA = new TabellMengde<Integer>();
-//		TabellMengde<Integer> kunHosB = new TabellMengde<Integer>();
-//		TabellMengde<Integer> totalt = new TabellMengde<Integer>();
-//		
-//		//Legger til hobbyene til a
-//		for (int i = 0; i < a.getHobby().antallElementer(); i++) {
-//			if (b.inneholderHobby(a.getHobby())) {
-//				felles.leggTil(i);
-//			} else {
-//				kunHosA.leggTil(i);
-//			}
-//			totalt.leggTil(i);
-//		}
-//		
-//		//Legger til hobbyene til b
-//		for (int i = 0; i < b.getHobby().antallElementer(); i++) {
-//			if (!a.inneholderHobby(b.getHobby())) {
-//				kunHosB.leggTil(i);
-//			}
-//			totalt.leggTil(i);
-//		}
-//		
-//		return felles.antallElementer() - (kunHosA.antallElementer() + kunHosB.antallElementer()) / totalt.antallElementer();
-//	}
-		int antallFelles = 0;
-		int antallKunHosA = 0;
-		int antallKunHosB = 0;
-		int antallTotalt = 0;
-		
-		for (int i = 0; i < a.getHobby().antallElementer(); i++) {
-			if (b.getHobby().inneholder(a.hentElement(i))) {
-				antallFelles++;
-			} else {
-				antallKunHosA++;
-			}
-			antallTotalt++;
-		}
-		
-		for (int i = 0; i < b.getHobby().antallElementer(); i++) {
-			if (!a.getHobby().inneholder(b.hentElement(i))) {
-				antallKunHosB++;
-			}
-			antallTotalt++;
-		}
-		
-		return antallFelles - (antallKunHosA + antallKunHosB) / antallTotalt;
+
+	public static double match(Person a, Person b) {
+		MengdeADT<String> snittAogB = a.getHobby().snitt(b.getHobby());
+		double antallFelles = snittAogB.antallElementer();
+		double antallKunHosDenEne = a.getHobby().minus(b.getHobby()).antallElementer();
+		double antallKunHosDenAndre = b.getHobby().minus(a.getHobby()).antallElementer();
+		double antallTotalt = a.getHobby().union(b.getHobby()).antallElementer();
+		double match = antallFelles - (antallKunHosDenEne + antallKunHosDenAndre) / antallTotalt;
+		return match;
 	}
-		
 }
-
-
