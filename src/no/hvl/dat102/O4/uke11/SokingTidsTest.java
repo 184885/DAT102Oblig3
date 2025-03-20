fd92ee674f18c82e888a33c7ddc1fffe1674a026
Leah <799043@stud.hvl.no>
2025-03-20 13:13:46 +0100

    søking fikset

--- src/no/hvl/dat102/O4/uke11/SokingTidsTest.java
+++ src/no/hvl/dat102/O4/uke11/SokingTidsTest.java
@@ -12,15 +12,17 @@ public class SokingTidsTest {
 	public static void main(String[] args) {
 		
 		// Antall
-		int antElement = 100000;
+		int antElement = 1000000;
 		
 		// Lager en tabell og et sett
 		Set<Integer> set = new HashSet<>(antElement);
 		Integer[] tabell = new Integer[antElement];
+		Integer[] sammenligningsTabell = new Integer[antElement];
 		
 		// Fyller tabell og sett med tilfeldige tall
 		for (int i = 0; i < antElement; i++) {
-			tabell[i] = rand.nextInt(1000000);
+			tabell[i] = rand.nextInt(100000);
+			sammenligningsTabell[i] = tabell[i];
 			set.add(tabell[i]);
 		}
 		
@@ -30,14 +32,14 @@ public class SokingTidsTest {
 		// Starter tiden og søker etter et tall i tabell og sett
 		Instant start = Instant.now();
 		for (int i = 0; i < 10000; i++) {
-			Binaersoker.binaertSokIterativ(tabell, tilfeldigtall(antElement));
+			Binaersoker.binaertSokIterativ(tabell, sammenligningsTabell[i]);
 		}
 		Instant stopp = Instant.now();
 		System.out.println("Tiden det tar å binærsøke " + 10000 + " elementer i tabell med " + antElement + " elementer:" + Duration.between(start, stopp).toString());
 		
 		Instant start1 = Instant.now();
 		for (int i = 0; i < 10000; i++) {
-			set.contains(tilfeldigtall(antElement));
+			set.contains(sammenligningsTabell[i]);
 		}
 		Instant stopp1 = Instant.now();
 		System.out.println("Tiden det tar å søke " + 10000 + " elementer i HashSet på " + antElement + " elementer:" + Duration.between(start1, stopp1).toString());
@@ -45,9 +47,9 @@ public class SokingTidsTest {
 	
 	// Genererer et tilfeldig tall
 		private static int tilfeldigtall(int antElement) {
-			int tall = rand.nextInt(1000000);
+			int tall = rand.nextInt(100000);
 	        for (int i = 0; i < antElement; i++) {
-	        	tall = (tall + 45713) % 1000000;
+	        	tall = (tall + 45713) % 100000;
 	        }
 	        return tall;
 	}
